67f18e10177efb7438f41c15b69df20685c75198
nikhilpatelrt31 <nikhilpatelrt31@gmail.com>
2025-04-27 11:55:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://img.freepik.com/free-photo/leather-texture-background_1385-1128.jpg?semt=ais_hybrid&w=740");
+        # background-image: url("https://img.freepik.com/free-photo/leather-texture-background_1385-1128.jpg?semt=ais_hybrid&w=740");
         # background-image: url("https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-position: center;
